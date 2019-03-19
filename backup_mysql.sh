ceba13e69a081adff8374985ca218feac3002b55
于庆宇 <yu@yuqingyudeMacBook-Pro.local>
2019-03-19 15:34:52 +0800

    a

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-DIR=/backup/mysql
+DIR=./backup/mysql
 if [ ! -e $DIR ]
 then
-/bin/mkdir -p $DIR
+mkdir -p $DIR
 fi
 
-
+# mysqldump
 docker-compose exec mysql mysqldump -uroot -p123456 management > "$DIR/data_`date +%Y%m%d`.sql"
 
-
-#查找更改时间在30日以前的sql备份文件并删除
+# 查找更改时间在30日以前的sql备份文件并删除
 find $DIR -mtime +30 -name "data_[1-9]*.sql" -exec rm -rf {} \;
 
-git add $DIR/*
+# git
+git add '$DIR/*'
 git commit -m 'backup'
 git push origin master
