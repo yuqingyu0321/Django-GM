d6c50272986dbf2ef139e19da56b770fb6521c4c
yuqingyu0321 <https://yuqingyu0321@github.com>
2019-03-19 17:26:15 +0800

    back.sh

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -9,10 +9,13 @@ fi
 # mysqldump
 docker-compose exec mysql mysqldump -uroot -p123456 management > "$DIR/data_`date +%Y%m%d`.sql"
 
+# 删除mysqldump错误
+sed -i '/mysqldump/d' "$DIR/data_`date +%Y%m%d`.sql"
+
 # 查找更改时间在30日以前的sql备份文件并删除
 find $DIR -mtime +30 -name "data_[1-9]*.sql" -exec rm -rf {} \;
 
 # git
-git add '$DIR/*'
+git add "$DIR/*"
 git commit -m 'backup'
 git push origin master
